8e2415af71c78ebb2990c2037680003f04a359e6
Ashish Gaurav <ashish.g308@gmail.com>
2021-05-29 14:57:58 +0000

    Updating event data

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.use(bodyParser.json());
 // from a cloud data store
 const mockEvents = {
     events: [
-        { title: 'an event', id: 1, description: 'something really cool' },
-        { title: 'another event', id: 2, description: 'something even cooler' }
+        { title: 'First Event', id: 1, description: 'This is first event' },
+        { title: 'Second Event', id: 2, description: 'This is second event' }
     ]
 };
 
